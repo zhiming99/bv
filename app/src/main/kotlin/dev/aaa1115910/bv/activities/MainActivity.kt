aaa2b83cb59ceff091732964ad482b3b81e2410b
zhiming <woodhead99@gmail.com>
2023-06-02 10:53:56 +0800

    . enable PRC

--- app/src/main/kotlin/dev/aaa1115910/bv/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/activities/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : ComponentActivity() {
             }
 
             BVTheme {
-                if (isMainlandChina) RegionBlockScreen() else HomeScreen()
+                HomeScreen()
             }
         }
     }
